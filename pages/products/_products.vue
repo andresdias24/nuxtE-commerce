d8ddc3183e91703b5b6d83f720e29d76dc90f71e
Harold <andresdias24@gmail.com>
2021-03-23 10:44:35 -0500

    make it better

---
+++ pages/products/_products.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="currentProduct.nombre">
+    <Nav class="sticky top-0" />
+    <h1 class="font-bold m-5 md:mx-10">
+      {{ currentProduct.nombre }}
+    </h1>
+    <Products :data="currentProduct" />
+    <Ads class="mx-auto sm:m-10" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $strapi, route }) {
+    const id = route.params.products
+    debugger;
+    const currentProduct = await $strapi.$productos.findOne(id)
+    debugger;
+    return { currentProduct }
+  },
+  data() {
+    return {
+      currentProduct: {},
+    }
+  },
+}
+</script>
+
+<style scoped></style>
